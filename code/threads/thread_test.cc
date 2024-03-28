1e838c148c975e9f9a83f7f661784d8939dc25c9
ZoeGranitto <zoegranitto@gmail.com>
2024-03-28 20:46:47 -0300

    barber test

--- code/threads/thread_test.cc
+++ code/threads/thread_test.cc
@@ -15,6 +15,8 @@
 #include "thread_test_prod_cons.hh"
 #include "thread_test_simple.hh"
 #include "thread_test_garden_sem.hh"
+#include "thread_test_barber.hh"
+
 #include "lib/utility.hh"
 
 #include <stdio.h>
@@ -32,7 +34,8 @@ static const Test TESTS[] = {
     { &ThreadTestSimple,   "simple",   "Simple thread interleaving" },
     { &ThreadTestGarden,   "garden",   "Ornamental garden" },
     { &ThreadTestProdCons, "prodcons", "Producer/Consumer" },
-    { &ThreadTestGardenSem, "gardensem", "Ornamental garden with semaphores"}
+    { &ThreadTestGardenSem, "gardensem", "Ornamental garden with semaphores"},
+    { &ThreadTestBarber, "barber", "Barber with multiple clients"}
 };
 static const unsigned NUM_TESTS = sizeof TESTS / sizeof TESTS[0];
 
---
+++ code/threads/thread_test_barber.hh
@@ -0,0 +1,11 @@
+#ifndef NACHOS_THREADS_THREADTESTBARBER__HH
+#define NACHOS_THREADS_THREADTESTBARBER__HH
+
+#include "synch_list.hh"
+#include "thread.hh"
+#include "system.hh"
+#include "semaphore.hh"
+
+void ThreadTestBarber();
+
+#endif
