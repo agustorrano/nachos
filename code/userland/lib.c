3b749aeb146b9afe23d5a69bc8ccf32a6d12d295
ZoeGranitto <zoegranitto@gmail.com>
2024-05-18 18:02:22 -0300

    itoa function

--- code/userland/lib.c
+++ code/userland/lib.c
@@ -12,4 +12,28 @@ void puts (const char *s)
   Write(s, strlen(s), CONSOLE_OUTPUT);
 }
 
-void itoa (int n , char *str);
+void reverse (char str[], int length)
+{
+  int start = 0;
+  int end = length - 1;
+  while (start < end) {
+    char temp = str[start];
+    str[start] = str[end];
+    str[end] = temp;
+    end--;
+    start++;
+  }
+}
+
+void itoa (int n , char *str) 
+{
+  int i = 0;
+  while (n != 0) {
+    int c = n % 10;
+    str[i++] = c + '0';
+    n = n / 10;
+  }
+  str[i] = '\0';
+  reverse(str, i);
+  return;
+}
