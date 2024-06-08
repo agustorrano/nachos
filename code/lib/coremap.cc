dfd07dd75b8a6d4089b7e6a207d737504379a23b
agustorrano <agustorrano2003@gmail.com>
2024-06-08 14:10:39 -0300

    coremap

--- code/lib/coremap.cc
+++ code/lib/coremap.cc
@@ -1,20 +1,68 @@
-
 #include <stdio.h>
 #include "coremap.hh"
 
-CoremapEntry::CoremapEntry()
+Coremap::Coremap(unsigned nitems)
 {
-  sid = -1;
-  virtualPageNumber = -1;
+    ASSERT(nitems > 0);
+    
+    frames = new Bitmap(nitems);
+    spaces = new AddressSpace*[nitems];
+    vpns = new unsigned[nitems];
+    coremapSize = nitems;
 }
 
-/*
-CoremapEntry::~CoremapEntry()
+Coremap::~Coremap()
 {
-} */
+    delete frames;
+    delete [] spaces;
+    delete [] vpns;
+}
+
+void
+Coremap::Mark(unsigned which, AddressSpace *addrSpace, unsigned vpn)
+{
+    frames->Mark(which);
+    spaces[which] = addrSpace;
+    vpns[which] = vpn;
+}
+
+void
+Coremap::Clear(unsigned which)
+{
+    frames->Clear(which);
+    spaces[which] = nullptr;
+
+    // que hago con los vpns? 
+    // los seteo en -1 o los dejo como estan?
+}
+
+bool
+Coremap::Test(unsigned which) const
+{
+    return frames->Test(which) && spaces[which] != nullptr;
+}
+
+int 
+Coremap::Find(AddressSpace *addrSpace, unsigned vpn)
+{
+    int which = frames->Find();
+    spaces[which] = addrSpace;
+    vpns[which] = vpn;
+    return which;
+}
+
+unsigned 
+Coremap::CountClear() const
+{
+    return frames->CountClear();
+}
 
 void 
-CoremapEntry::Print() 
+Coremap::Print() 
 {
-  printf("Address Space: %d. Virtual Page Number: %d.\n", sid, virtualPageNumber);
+    for (unsigned i = 0; i < coremapSize; i++) {
+        if (Test(i)) {
+            printf("Address Space: %p. Virtual Page Number: %d.\n", spaces[i], vpns[i]);
+        }
+    }
 }
--- code/lib/coremap.hh
+++ code/lib/coremap.hh
@@ -3,20 +3,47 @@
 
 
 #include "syscall.h"
+#include "bitmap.hh"
+#include "utility.hh"
+#include "userprog/address_space.hh"
 
-class CoremapEntry {
+class Coremap {
 public:
-    // The identification number for the addr space where the physical page belongs
-    SpaceId sid;    
-    
-    /// The page number in virtual memory. en realidad creo que aloja varias, como seria?
-    unsigned virtualPageNumber;
+    //// Initalize a coremap with `nitems` bits.
+    Coremap(unsigned nitems);
+
+    /// Uninitalize a coremap.
+    ~Coremap();
+
+    /// Set de "nth" bit.
+    void Mark(unsigned which, AddressSpace *addrSpace, unsigned vpn);
+
+    /// Clear the "nth" bit.
+    void Clear(unsigned which);
+
+    /// Is the "nth" bit set?
+    bool Test(unsigned which) const;
+
+    /// It finds a physical frame.
+    int Find(AddressSpace *addrSpace, unsigned vpn);
+
+    /// Return the number of clear bits.
+    unsigned CountClear() const;
 
     /// Print contents of coremap entry.
     void Print();
 
 private:
 
+    Bitmap *frames;
+
+    // The identification number for the addr space where the physical page belongs
+    AddressSpace **spaces;   
+    
+    /// The page number in virtual memory. en realidad creo que aloja varias, como seria?
+    unsigned *vpns;
+
+    unsigned coremapSize;
 };
 
 
