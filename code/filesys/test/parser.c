b06418981f40fa8b2e1aac55524540bda80c6034
ZoeGranitto <zoegranitto@gmail.com>
2024-06-29 13:28:35 -0300

    parser ej 4

---
+++ code/filesys/test/parser.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int parseDir(char* string, char* outName, char* outDir[10]) {
+    char *delim = "/";
+    int ntok = 0;
+    char *saveptr;
+    char *first = strtok_r(string, delim, &saveptr);
+    outDir[ntok] = first;
+    for (char *token = strtok_r(NULL, delim, &saveptr); token != NULL; token = strtok_r(NULL, delim, &saveptr))
+    {
+      ntok++;
+      outDir[ntok] = token;
+    }    
+    strcpy(outName, outDir[ntok]);
+    outDir[ntok] = NULL;
+    return ntok;
+}
+
+int main() {
+    char ruta[] = "./file";
+    char nombre[10];
+    char *directorios[10] = {NULL};
+
+    int cantDir = parseDir(ruta, nombre, directorios);
+
+    printf("Nombre del archivo: %s\n", nombre);
+
+    printf("Directorios: ");
+    for (int i = 0; i < cantDir; i++)
+      printf("[%s] ", directorios[i]);
+    printf("\n");
+    return 0;
+}
